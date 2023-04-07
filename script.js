1447f4a6276e8b9b6305dee3d244022d094a60c2
AK <70256065+Aarush-new@users.noreply.github.com>
2023-04-07 18:38:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,4 +27,44 @@ $(document).ready(function() {
       });
     });
   });
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const apiKey = 'e882b31d379fb898583148de637d0a83'; // replace with your own API key
+  
+    $.getJSON(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`, function(data) {
+      const weatherOutput = `
+        <h2>Weather for ${data.name}, ${data.sys.country}</h2>
+        <div class="weather-info">
+          <div class="left-col">
+            <p>Temperature: ${Math.round(data.main.temp - 273.15)}°C</p>
+            <p>Pressure: ${data.main.pressure} hPa</p>
+            <p>Sea Level: ${data.main.sea_level ? data.main.sea_level : 'N/A'}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Description: ${data.weather[0].description}</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+            <p>Latitude: ${data.coord.lat}</p>
+            <p>Longitude: ${data.coord.lon}</p>
+          </div>
+          <div class="right-col">
+            <p><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"></p>
+          </div>
+        </div>
+      `;
+      $('#weatherOutput').html(weatherOutput);
+    });
+  }
+  
+  
+
+
   
